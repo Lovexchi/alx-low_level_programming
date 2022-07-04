4d480f23013204af349abc0e2af643d1e32873a3
lovexchi <dovewhite070@gmail.com>
2022-07-04 04:09:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-#include <stdio.h>/*** * main - Write '"Programming is like building a multilingual puzzle'* *Return: Always 0 (success)* */int main(vod){puts("\"Programming is like building a multilingual puzzle");return (0);}
+ #include <stdio.h>
+/**
+ * * main - Write '"Programming is like building a multilingual puzzle'
+ * *Return: Always 0 (success)
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
